6e6d7a2eb5732ece80e02e24ce09a8ff887eb0c6
Charanjot Kaur <118251729+Ckaur6229@users.noreply.github.com>
2024-11-03 11:33:52 +0530

    Fresh Commit

--- src/components/layouts/AdminMaster.js
+++ src/components/layouts/AdminMaster.js
@@ -3,7 +3,6 @@ import {Navigate, Outlet } from 'react-router-dom'
 import AdminHeader from './AdminHeader'
 import { toast } from 'react-toastify'
 import AdminFooter from './AdminFooter'
-const email=sessionStorage.getItem("email")
 export default function AdminMaster() {
   const email=sessionStorage.getItem("email")
   if(!email){
